21a13fa1ac4fdf569aab2649c9b8254852a98238
Ozéias Lima <ozeiasdsl@gmail.com>
2021-02-27 22:31:41 -0300

    another test

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 8080
-const fs = require('fs')
-const bodyParser = require('body-parser');
+function Home() {
+    return <div>Home</div>
+}
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+const getData = () => {
+        
+    const req = new XMLHttpRequest();
+    req.open('GET', ' app.js');
 
-app.post('/orders',function(req,res){
+    req.responseType = 'json';
 
-    var reqData =  JSON.stringify(req.body.data);
+    req.onload  = () => {
+        const data = req.response;
+        console.log(data);
+    }
+    req.send();
 
-    console.log(reqData);
+}
+getData();
 
-    storeData(reqData, "/orders");
-
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(data));
-    res.end();
-
-});
-
-app.get("/", (req, res) => {
-    console.log("Root requested");
-    res.end('Hello World!');
-});
-
-app.get("/orders", (req, res) => {
-
-    const orders = () => fs.readFileSync(__dirname + '/orders.json', { endoding: 'utf8'})
-
-    const ordersRead = JSON.parse(orders())
-
-    /*Logging for visualization*/
-    res.end(JSON.stringify(ordersRead));
-    console.log("Orders list requested")
-});
-
-
-app.post("/orders", (req, res) => {
-
-    fs.writeFile(__dirname + '/orders.json', JSON.stringify(data), function writeJSON(err) {
-        if (err) return console.log(err);
-        console.log(JSON.stringify(data));
-      });
-
-    console.log("Orders list updated")
-
-});
-
-
-app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`)
-  });
-
-
-// Functions
+export default Home

7fad62dc69f810e18ba49b34fc00519628024a9f
Ozéias Lima <ozeiasdsl@gmail.com>
2021-02-27 22:16:09 -0300

    atualizado

--- pages/index.js
+++ pages/index.js
@@ -2,4 +2,20 @@ function Home() {
     return <div>Home</div>
 }
 
+const getData = () => {
+        
+    const req = new XMLHttpRequest();
+    req.open('GET', ' https://old-cat-9.loca.lt');
+
+    req.responseType = 'json';
+
+    req.onload  = () => {
+        const data = req.response;
+        console.log(data);
+    }
+    req.send();
+
+}
+getData();
+
 export default Home
